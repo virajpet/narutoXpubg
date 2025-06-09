1c127db909d04daf596d593a2ef3298f76c069ff
virajpet <virajpet@users.noreply.github.com>
2025-06-09 12:55:05 +0530

    added backend deployment link to frontend

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import { Character } from "../data/characters";
 
-const API_BASE_URL = "http://localhost:3002";
+const API_BASE_URL = "https://narutoxpubg.onrender.com";
 
 // Response interface for the Node.js backend
 interface ApiResponse<T> {
